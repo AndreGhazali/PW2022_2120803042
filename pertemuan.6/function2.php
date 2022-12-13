67f41ef397dc69d6360d6fbedc82c5ac368ea1de
AndreGhazali <andreghazali0803@gmail.com>
2022-12-14 05:34:23 +0700

    Izin mengumpulkan tugas yang di pertemuan 13 pak

--- pertemuan.6/function2.php
+++ pertemuan.6/function2.php
@@ -4,6 +4,6 @@ $tulisan = "Selamat datang di kuliah pemrograman web 2";
 
 echo substr($tulisan, 8) . "<br>";
 echo substr($tulisan, 8, 6) . "<br>";
-echo substr($tulisan, 8-17 . "<br>";
+echo substr($tulisan, 8-17) . "<br>";
 
 ?>
